5ff86109de9401a296e29ef0f26e2f4f6475cba4
HAHWUL <hahwul@gmail.com>
2024-02-19 00:59:27 +0900

    first commit

---
+++ lib/jekyll/securitytxt.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "securitytxt/version"
+
+module Jekyll
+  module Securitytxt
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
