fe5128a4375884aa89ab59447bb958f5735e0255
HAHWUL <hahwul@gmail.com>
2024-11-22 23:42:16 +0900

    Refactor security.txt generator for clarity and consistency
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- lib/jekyll/securitytxt.rb
+++ lib/jekyll/securitytxt.rb
@@ -5,7 +5,7 @@ require "jekyll"
 
 module Jekyll
   module Securitytxt
-    # Jekyll plugin for security.txt
+    # Jekyll plugin for generating security.txt
     class Generator < Jekyll::Generator
       safe true
       priority :lowest
@@ -13,35 +13,37 @@ module Jekyll
       # Plugin entry point.
       def generate(site)
         @site = site
-        @site.pages << verification_file unless a_verification_file_exist?
+        @site.pages << create_security_txt_file unless security_txt_file_exists?
       end
 
       private
 
       # Check if a security.txt file already exists in the source tree.
-      def a_verification_file_exist?
+      def security_txt_file_exists?
         file_pattern = %r{.well-known/security\.txt?}
         exists = @site.static_files.any? { |p| p.url =~ file_pattern }
-        warn_msg = "Found a security.txt file in source tree matching /#{file_pattern.source}/; not generating one..."
-        Jekyll.logger.warn "Jekyll-securitytxt", warn_msg if exists
+        if exists
+          Jekyll.logger.warn "Jekyll-securitytxt",
+                             "Found a security.txt file in source tree matching /#{file_pattern.source}/; not generating one..."
+        end
         exists
       end
 
       # Get path of the template file.
-      def source_path(file = "template.html")
-        File.expand_path file.to_s, __dir__
+      def template_file_path(file = "template.html")
+        File.expand_path(file.to_s, __dir__)
       end
 
       # Path of the security.txt file.
-      def securitytxt_path
+      def security_txt_file_path
         ".well-known/security.txt"
       end
 
       # Construct a file object from a template with content that can be added to generated pages
-      def verification_file
-        Jekyll.logger.info "Jekyll-securitytxt", "Generating #{securitytxt_path}"
-        page = PageWithoutAFile.new(@site, __dir__, "", securitytxt_path)
-        page.content = File.read(source_path)
+      def create_security_txt_file
+        Jekyll.logger.info "Jekyll-securitytxt", "Generating #{security_txt_file_path}"
+        page = PageWithoutAFile.new(@site, __dir__, "", security_txt_file_path)
+        page.content = File.read(template_file_path)
         page.data["layout"] = nil
         page
       end
